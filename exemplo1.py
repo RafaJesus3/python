0e8dfc6f7b900cd43b352feb2e07acc1ec547dab
RafaJesus3 <rafael.sjesus1@outlook.com>
2026-09-02 20:53:06 -0300

    Aula python: Dois arquivos adicioandos

---
+++ exemplo1.py
@@ -0,0 +1,9 @@
+# Exemplo de Entrada-Processamento-Saida
+
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+print(nome)
+print(idade)
+print(type(nome))
+print(type(idade))
+# print(nome) -> Exemplo de código comentado
